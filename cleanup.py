c3a701aa422844b9fade8e29166c8169f2427646
chu.tq <chu.tq@teko.vn>
2024-04-14 17:23:25 +0700

    init

---
+++ cleanup.py
@@ -0,0 +1,26 @@
+import os
+
+SIZE_IN_MB = 1024 * 1024
+
+
+def get_size(dir_path):
+    total_size = 0
+    for dirpath, dirnames, filenames in os.walk(dir_path):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            total_size += os.path.getsize(fp)
+    return total_size
+
+
+def print_large_directories(start_path, size_in_mb, indent_level, max_depth):
+    for directory in os.listdir(start_path):
+        dir_path = os.path.join(start_path, directory)
+        size = get_size(dir_path)
+        if size > size_in_mb * SIZE_IN_MB:  # size > 300MB
+            print(f"{indent_level*'__'}Directory: {dir_path}, Size: {size / (1024 * 1024)} MB")
+            if indent_level < max_depth-1:
+                print_large_directories(dir_path, size_in_mb/3, indent_level+1, max_depth)
+
+
+# Call the function with the directory path
+print_large_directories("M:/HK5/XL_THS", 300, 0,3)
